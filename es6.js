2b9223a8105064d3afcac7c7a34d2bad68ac3e5b
Halina Klyashtornaya <128702688+Halina2610@users.noreply.github.com>
2023-06-25 22:52:20 +0300

    Update es6.js

--- es6.js
+++ es6.js
@@ -509,6 +509,22 @@ console.log(greeting);
   '<li class="text-warning">linebreak</li>'
 ]*/
 
+const result = {
+  success: ["max-length", "no-amd", "prefer-arrow-functions"],
+  failure: ["no-var", "var-on-top", "linebreak"],
+  skipped: ["no-extra-semi", "no-dup-keys"]
+};
+function makeList(arr) {
+  // Only change code below this line
+  const failureItems = arr.map(item => `<li class="text-warning">${item}</li>`); //[ получим список '<li class="text-warning">no-var</li>','<li class="text-warning">var-on-top</li>', '<li class="text-warning">linebreak</li>' ]
+  
+  // Only change code above this line
+
+  return failureItems;
+}
+
+const failuresList = makeList(result.failure);
+
 //task 16
 /**/
 
